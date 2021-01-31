475a22abde56e8388a138861d5ea8013ee58b776
AUNGKYAWCHA MARMA <aungkyawcha@AUNGKYAWCHAsMBP.lan>
2021-01-31 16:18:07 -0500

    started recursion 1

---
+++ recursion-1/PrintReverse.java
@@ -0,0 +1,14 @@
+class Solution{
+    public static void printReserve(String str) {
+        helper(str, 0);
+    }
+
+    // to optimize the stack, make these methods as instance methods and have an instance varible of string, assign user input string to that variable and access that from the helper, instead of passing it in every recursive call
+    public static void helper(String str, int index) {
+        if (str == null || index >= str.length()) {
+            return;
+        }
+        helper(str, index + 1);
+        System.out.println(str.charAt(index));
+    }
+}
